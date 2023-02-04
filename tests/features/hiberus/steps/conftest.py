6a4311b05ca68d3657a77f2167ecc82a5304a7f4
Matias Ceraño <matiemanuel@gmail.com>
2023-02-04 17:34:51 -0300

    Adding docker files and remote web driver configuration

--- tests/features/hiberus/steps/conftest.py
+++ tests/features/hiberus/steps/conftest.py
@@ -16,20 +16,32 @@ def config_browser():
     browser = config['browser']
     headless = config['headless']
     implicit_time = config['implicit_wait']
+    run_remotely = config['run_remotely']
+    command_executor = config['command_executor']
 
-    if browser == 'Firefox':
+    if browser.lower() == 'firefox':
         firefox_options = selenium.webdriver.FirefoxOptions()
         firefox_options.add_argument('--private')
         if headless:
             firefox_options.add_argument('--headless')
-        b = selenium.webdriver.Firefox(options=firefox_options)
+        if run_remotely:
+            b = selenium.webdriver.Remote(command_executor=command_executor, options=firefox_options)
+        else:
+            b = selenium.webdriver.Firefox(options=firefox_options)
 
-    elif browser == 'Chrome':
+    elif browser.lower() == 'chrome':
         opts = selenium.webdriver.ChromeOptions()
         opts.add_argument('incognito')
+        opts.add_argument('no-sandbox')
+        opts.add_argument('disable-dev-shm-usage')
+        opts.add_argument('disable-gpu')
+        opts.add_argument('window-size=1920,1080')
         if headless:
             opts.add_argument('headless')
-        b = selenium.webdriver.Chrome(options=opts)
+        if run_remotely:
+            b = selenium.webdriver.Remote(command_executor=command_executor, options=opts)
+        else:
+            b = selenium.webdriver.Chrome(options=opts)
     else:
         raise Exception(f"Browser {browser} is not supported")
     b.implicitly_wait(implicit_time)
